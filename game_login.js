744e66be5b4110ed25e446cce034057eae469b86
LeoBatata <149201381+LeoBatata@users.noreply.github.com>
2024-03-05 18:45:02 -0300

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function addUser() {
+    console.log("error")
+    player1Name = document.getElementById("nomeInput1").value;
+    player2Name = document.getElementById("nomeInput2").value;
+
+    localStorage.setItem("player1Name", player1Name);
+    localStorage.setItem("player2Name", player2Name);
+
+    window.location = "game_page.html"
+}
